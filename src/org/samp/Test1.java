b9721e4514892a7f14a28760dbe3d09088bbd3cc
subburaj036 <subburaj036@gmail.com>
2022-12-07 19:34:05 +0530

    update

---
+++ src/org/samp/Test1.java
@@ -0,0 +1,13 @@
+package org.samp;
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println("subbu");
+		
+		System.out.println("hari");
+		
+		System.out.println("ms");
+		
+	}
+
+}
