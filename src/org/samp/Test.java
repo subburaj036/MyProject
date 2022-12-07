c84397e8a92a4c2845da64c1d78aa8df2a7f800a
subburaj036 <subburaj036@gmail.com>
2022-12-07 19:24:54 +0530

    sample

---
+++ src/org/samp/Test.java
@@ -0,0 +1,12 @@
+package org.samp;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("test1");
+		
+		System.out.println("java");
+		
+		System.out.println("selenium");
+	}
+
+}
